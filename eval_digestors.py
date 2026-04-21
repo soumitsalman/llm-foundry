7396094e2deaf7db3a9bcd127afc38a445360526
Soumit Salman Rahman <soumitsr@gmail.com>
2026-04-21 18:40:49 +0000

    Refactor main function to accept model name as a parameter and reorganize imports for clarity.

--- eval_digestors.py
+++ eval_digestors.py
@@ -1,9 +1,7 @@
-from vllm import LLM, SamplingParams
-from transformers import AutoTokenizer
 from models import *
 from utils import *
-
-MODEL_NAME = "Qwen/Qwen3.5-0.8B"
+from icecream import ic
+import json, yaml
 
 SYS_MSG = f"""
 EXTRACT {','.join(NewsSummaryBase.model_fields.keys())} FROM INPUT INTO raw_response
@@ -18,21 +16,26 @@ create_msg = lambda text: [
     { "role": "user", "content": INST_MSG.format(text) }
 ]
 
-
 def load_inputs():
     return [create_msg(item['text']) for item in load_data_from_directory("data/digests/*.json")[:128]]
 
-def main():
-    tokenizer = AutoTokenizer.from_pretrained(MODEL_NAME)
+from vllm import LLM, SamplingParams
+from transformers import AutoTokenizer
+
+def main(model_name):
+
+    
+    tokenizer = AutoTokenizer.from_pretrained(model_name)
     create_text = lambda messages: tokenizer.apply_chat_template(messages, tokenize=False, add_generation_prompt=True, enable_thinking=False)
     texts = list(map(create_text, load_inputs()))
-    llm = LLM(MODEL_NAME)
+    llm = LLM(model_name)
     sampling_params = SamplingParams(temperature=0.3, top_p=0.95, max_tokens=512)
     outputs = llm.generate(texts, sampling_params=sampling_params)
-    data = [json.loads(t.strip().removeprefix("```json").removeprefix("```").removesuffix("```")) for t in outputs]
+    data = [json.loads(t.outputs[0].text.strip().removeprefix("```json").removeprefix("```").removesuffix("```")) for t in outputs]
     with open(".test/outputs.json", "w") as f:
         json.dump(data, f, indent=2)
 
+MODEL_NAME = "Qwen/Qwen3.5-0.8B"
 if __name__=="__main__":
-    main()
+    main(MODEL_NAME)
 
